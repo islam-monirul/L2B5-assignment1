82e8827cbe405ce423206d4966c0861aca818f0f
islam-monirul <islammonirul.cse@gmail.com>
2025-05-09 00:49:34 -0230

    problem 1 solved

---
+++ assignment-solutions.ts
@@ -0,0 +1,4 @@
+function formatString(input: string, toUpper?: boolean): string {
+	if (toUpper === true || toUpper === undefined) return input.toUpperCase();
+	else return input.toLocaleLowerCase();
+}
