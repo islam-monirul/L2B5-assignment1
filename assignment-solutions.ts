2b8514cab00cceb26103b8b1b75d3956d2440c7c
islam-monirul <islammonirul.cse@gmail.com>
2025-05-09 12:51:18 -0230

    problem 7 solved

--- assignment-solutions.ts
+++ assignment-solutions.ts
@@ -71,3 +71,18 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
 		return mostExpensive;
 	}
 }
+
+enum Day {
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+	Sunday,
+}
+
+function getDayType(day: Day): string {
+	if (day === Day.Saturday || day === Day.Sunday) return "Weekend";
+	else return "Weekday";
+}
