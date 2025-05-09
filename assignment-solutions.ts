c6f69380c68740940c06e3a5c90cdee4cc59c524
islam-monirul <islammonirul.cse@gmail.com>
2025-05-09 00:56:30 -0230

    problem 2 solved

--- assignment-solutions.ts
+++ assignment-solutions.ts
@@ -2,3 +2,12 @@ function formatString(input: string, toUpper?: boolean): string {
 	if (toUpper === true || toUpper === undefined) return input.toUpperCase();
 	else return input.toLocaleLowerCase();
 }
+
+function filterByRating(
+	items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+	// using array filter method to get the desired items with rating of 4 or more
+	return items.filter((singleItem) => {
+		return singleItem.rating >= 4;
+	});
+}
