fe4bf93650886428b272a2113ec7be0944b65d32
islam-monirul <islammonirul.cse@gmail.com>
2025-05-09 12:29:12 -0230

    problem 4 solved

--- assignment-solutions.ts
+++ assignment-solutions.ts
@@ -21,10 +21,34 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 	return concatenatedArray;
 }
 
+class Vehicle {
+	private make: string;
+	private year: number;
+
+	constructor(make: string, year: number) {
+		this.make = make;
+		this.year = year;
+	}
+
+	getInfo() {
+		return `Make: ${this.make}, Year: ${this.year}`;
+	}
+}
+
+class Car extends Vehicle {
+	private model: string;
+
+	constructor(make: string, year: number, model: string) {
+		super(make, year);
+		this.model = model;
+	}
+
+	getModel() {
+		return `Model: ${this.model}`;
+	}
+}
+
 function processValue(value: string | number): number {
 	if (typeof value === "string") return value.length;
 	else return value * 2;
 }
-
-console.log(processValue("hello"));
-console.log(processValue(10));
